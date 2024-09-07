f778aab6ce0e6e185cbeff64eba4008ed8cda282
Hanna Hummal <hanna.hummal@gmail.com>
2024-09-07 19:38:15 +0300

    ex21, %, &&, ||

---
+++ src/week1/ex21.java
@@ -0,0 +1,19 @@
+package week1;
+import java.util.Scanner;
+
+public class ex21 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type a year: ");
+        int year = reader.nextInt();
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("The year is a leap year");
+        }   else {
+            System.out.println("The year is not a leap year");
+        }
+    }
+}
+
+//if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
